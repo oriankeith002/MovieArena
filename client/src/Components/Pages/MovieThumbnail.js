117dba8571d160d949196b9215a34ec744361ea9
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-19 00:53:50 +0300

    Movie Thumbnail with border

--- client/src/Components/Pages/MovieThumbnail.js
+++ client/src/Components/Pages/MovieThumbnail.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Thumb from '../assets/test.jpg'
-
+import './MovieThumbnail.css'
 
 const MovieThumbnail = () => {
   return (
     <div className='thumb-container home-content'>
         <a href='ds' className='browser-movie-link'>
             <figure>
-                <img className='img-responsive' src={Thumb} alt="movie-id" />
+                <img 
+                  className='img-responsive' 
+                  src={Thumb} 
+                  alt="movie-id" 
+                  style={{width:210, height:315}}
+                />
             </figure>
         </a>
     </div>

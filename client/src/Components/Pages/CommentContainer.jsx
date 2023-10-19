49100ad8fcb4a32c8dd9e6a49987b00d6bb01f6f
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-19 20:39:49 +0300

    Added comment box section

---
+++ client/src/Components/Pages/CommentContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './CommentContainer.css'
+
+const CommentContainer = () => {
+  return (
+    <div className="comment-box">
+        <img src=''alt='' className='commenter-img' />
+        <div className='comment-info'>
+            <h3>Jameson Rwakaka</h3>
+            <p>
+                Lorem ipsum dolor sit amet 
+                consectetur adipisicing elit. 
+                Ad, quo.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default CommentContainer;

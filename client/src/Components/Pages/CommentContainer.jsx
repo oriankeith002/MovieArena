7aeb1d44e7159485c1d9c2fd65718952280569e1
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-20 17:15:05 +0300

    Responsive Movie Detail Page and Comment Container

--- client/src/Components/Pages/CommentContainer.jsx
+++ client/src/Components/Pages/CommentContainer.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './CommentContainer.css'
 
-const CommentContainer = () => {
+const CommentContainer = (props) => {
+  const {c_img, comment_date} = props;
   return (
     <div className="comment-box">
-        <img src=''alt='' className='commenter-img' />
+        <img src={c_img} alt='commentor_photo' className='commentor-img' />
         <div className='comment-info'>
-            <h3>Jameson Rwakaka</h3>
+            <h3>Jameson Rwakaka</h3> 
+            <span>{comment_date}</span>
             <p>
                 Lorem ipsum dolor sit amet 
                 consectetur adipisicing elit. 
                 Ad, quo.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, dicta. Tenetur ipsa modi labore inventore? Labore asperiores facere dolore minima voluptatibus atque perspiciatis et! Pariatur praesentium totam exercitationem doloremque ducimus.
             </p>
         </div>
     </div>
--- client/src/Components/Pages/MoviePage.jsx
+++ client/src/Components/Pages/MoviePage.jsx
@@ -1,25 +1,56 @@
 import React from 'react'
 import './MoviePage.css'
 import MovieThumbnail from './MovieThumbnail'
+import CommentContainer from './CommentContainer'
+import prof from '../assets/images.jpg'
+
 
 const MoviePage = () => {
+  let DateTest = new Date();
+  let formattedDate = DateTest.toLocaleDateString('en', {
+    day: 'numeric',
+    month: 'short',
+    year:'numeric'
+  }) ;
+  let formattedTime = DateTest.toLocaleTimeString('en',{
+    hour:'numeric',
+    minute:'numeric'
+  })
+
+  
+  console.log(DateTest)
+  console.log('changing ...')
+  console.log(formattedDate + ' at ' + formattedTime);
+
+  const commentEntryTime = formattedDate + ' at ' + formattedTime
+  const movieUploadEntryTime = formattedDate + ' at ' + formattedTime
   return (
     <div className='movie-page-container'>
-      <div className='two-column-section'>
+      <section className='the-movie-section'>
+        <div className='two-column-section'>
+          <div className='selected-movie-info'>
+            <div className='movie-graphic'>
+              <MovieThumbnail />
+            </div>
+            <div className='movie-info'>
+              <h1>The Childe</h1>
+              <h2>2023</h2>
+              <h2>Action/Drama/Thriller</h2>
+            </div>
+          </div>
+            
+          <div className='four-other-movies'>
+            <MovieThumbnail style={{width:105, height: 157}}/>
+            <MovieThumbnail style={{width:105, height: 157}}/>
+            <MovieThumbnail style={{width:105, height: 157}}/>
+            <MovieThumbnail style={{width:105, height: 157}}/>
+          </div>
+        </div>
+        <div className="movie-bg"></div>
+      </section>
+    
 
-        <div className='selected-movie-info'>
-          <MovieThumbnail />
-          <div className='movie-info-and-thumbnail'>thumbnail</div>
-        </div>
-          
-        <div className='four-other-movies'>
-          <MovieThumbnail style={{width:105, height: 157}}/>
-          <MovieThumbnail style={{width:105, height: 157}}/>
-          <MovieThumbnail style={{width:105, height: 157}}/>
-          <MovieThumbnail style={{width:105, height: 157}}/>
-        </div>
-      </div>
-      <div className='plot-summary-section'>
+      <section className='plot-summary-section'>
         <h2>Plot</h2>
         <p>
           Lorem, ipsum dolor sit amet consectetur 
@@ -29,28 +60,30 @@ const MoviePage = () => {
           esse voluptatum quod fugit earum saepe facilis 
           ipsum nobis repellat deserunt, suscipit eos ad eveniet. Veritatis autem cumque numquam corrupti, minima quisquam sed repudiandae officia aperiam quos amet repellat.
         </p>
+        <p className='uploader'>
+          <br />
+          <em>Uploaded by: <a href='X'>FREEMAN</a> </em>
+          <br />
+          <span>
+            <em>{movieUploadEntryTime}</em>
+          </span>
+        </p>
+      </section>
+      <section className='comment-section'>
+        <h2>17 Comments</h2>
+        <CommentContainer 
+          c_img={prof}
+          comment_date={commentEntryTime}
+        />
+        <CommentContainer 
+          c_img={prof}
+          comment_date={commentEntryTime}
+        />
+        <button className='add-comment-btn'> Login to leave a comment </button>
+      </section>
+    
 
-        <div className='uploaded-by-and-date'>
-          <div><em>Uploaded By </em> : Orian keith <br/></div>
-          <div><em>Date </em> : 15th October 2023</div>
-        </div>
-      </div>
-      <div className='comment-container'> 
-      comments here
-        <div className='comment-count'></div>
-        <div className='comment-box'>
-          <div className='main-comment-box'>
-            <div>
-              {/* this is a flex box kind of thing or grid  */}
-              <div className='commenter-name-initials'>AJ</div>
-              <div className='commenter-full-name'></div>
-              <div className='comment-made-at-time'></div>
-            </div>
-            <div className='the-comment-itself'></div>
-          </div>
-          <div className='comment-like-button'></div>
-        </div>
-      </div>
+      
     </div>
   )
 }

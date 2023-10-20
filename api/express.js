49e22a49c170e79c3ae5252cc43ad673aa3e3a34
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-20 17:53:54 +0300

    Setting up the api routes

--- api/express.js
+++ api/express.js
@@ -1,1 +1,22 @@
-const express = require('express');
+const express = require('express'); 
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+const userRoutes = require('./routes/user.routes');
+const authRoutes = require('./routes/auth.routes');
+const movieRoutes = require('./routes/movie.routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/apiv1/user', userRoutes);
+app.use('/apiv1/user', authRoutes);
+app.use('/apiv1/movies', movieRoutes);
+
+
+app.get('/test', (req,res) => {
+    res.json('test ok')
+}) 
+
+module.exports = app;

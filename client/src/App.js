476aa8638647943e29104eb76a4e792425c27bd5
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-23 10:44:09 +0300

    Adding logged in user to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,27 @@ import Register from './Components/Pages/UserAuthPages/Register';
 import MoviePage from './Components/Pages/MovieDetail/MoviePageDetail/MoviePage';
 import Landing from './Components/Pages/LandingPage/Landing';
 import MovieForm from './Components/Pages/MovieForm/MovieForm';
+import { UserContextProvider } from './Components/SupportUtilities/UserContext';
+
+
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Landing />} />
-        <Route path="/movie/:id" element={<MoviePage />} />
-        <Route path="/movie/new" element={<MovieForm />} />
-        <Route path="/movie/update/:id" element={<MovieForm />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="/movie/:id" element={<MoviePage />} />
+          <Route path="/movie/new" element={<MovieForm />} />
+          <Route path="/movie/update/:id" element={<MovieForm />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
--- client/src/Components/Header/UserProfDetail.jsx
+++ client/src/Components/Header/UserProfDetail.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './UserProfDetail.css';
+import { UserContext }  from '../SupportUtilities/UserContext';
+import {Link} from "react-router-dom";
 
 const UserProfDetail = ({src}) => {
+  const {user} = useContext(UserContext
+    )
   return (
-    <img src={src} alt='user-profile-img' className='nav-img' />
+    <Link to={ user?'/useraccount':'/login'}>
+      <img src={src} alt='user-profile-img' className='nav-img' />
+      {!!user && (
+        <div>
+          {user.name}
+        </div>
+      )}
+    </Link>
   )
 }
 

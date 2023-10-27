9c843eba2fb8cf0ab8d5ceb39a9f3c63bddb06de
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-27 16:04:39 +0300

    testings and verification

--- client/src/Components/Pages/LandingPage/Landing.jsx
+++ client/src/Components/Pages/LandingPage/Landing.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Landing.css';
 import UserAndMoviesCard from './UserAndMoviesCard';
 import { useNavigate } from 'react-router-dom';
+import { UserContext }  from '../../SupportUtilities/UserContext';
+
 
 const Landing = () => {
-  const navigate = useNavigate()
-  //refresh
-  navigate(0)
+  // const navigate = useNavigate()
+  // const {user} = useContext(UserContext) 
+
+  // //refresh
+  // const refreshPage = () => {
+  //   navigate(0)
+  // }
+
+  // if (user) {
+  //   refreshPage()
+  // }
+  
   return (
     <main className='landing-page-container'>
       <UserAndMoviesCard />
